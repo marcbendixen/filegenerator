6e78f5de7380e3982736fb672a601da53a18d4fb
Marc Bendixen <moin@marcbendixen.de>
2021-05-26 15:29:03 +0200

    Remove files

--- Header.spec.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-  import Header from './Header'
-  describe('Header', () => {
-    it('should render componente with Header', () => {
-      ...
-    })
-  }})
--- Header.stories.js
+++
@@ -1,5 +0,0 @@
-import Header from './Header'
-  export default {
-    title: 'Header'
-    component: Header
-  }
