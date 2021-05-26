b609bea4395ad82c5855e5ba7ed2e3883cfdcac6
Marc Bendixen <moin@marcbendixen.de>
2021-05-26 11:36:08 +0200

    Add basic setup for readline

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const writeFile = require('./writeFile')
+const readline = require('readline')
 
-const functionName = process.argv[2] ?? 'myFunction'
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-writeFile(functionName)
+rl.question('Which function and file would you like to create? ', answer => {
+  console.log(answer)
+  rl.close()
+})
