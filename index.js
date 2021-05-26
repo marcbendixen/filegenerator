b7e7446e67b118208b786c49e1e432827b4338fa
Marc Bendixen <moin@marcbendixen.de>
2021-05-26 14:29:49 +0200

    Updates

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const questions = [
     message:
       'Which function and file would you like to create? \nPlease seperate by comma.',
   },
+  {
+    type: 'checkbox',
+    message: 'Please select a file type ',
+    name: 'fileTypes',
+    choices: [{ name: 'Component' }, { name: 'Spec' }, { name: 'Stories' }],
+    validate(answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one file type.'
+      }
+      return true
+    },
+  },
 ]
 
 inquirer.prompt(questions).then(answers => {
   const files = answers['file'].split(',').map(element => element.trim())
   files.forEach(file => writeFile(file))
+  console.log(answers)
 })
