f3e4db1172cce8fff3f2dcc7c44820ea895fb59e
Marc Bendixen <moin@marcbendixen.de>
2021-05-26 15:44:15 +0200

    Updates to templates and text

--- index.js
+++ index.js
@@ -8,23 +8,28 @@ const templates = {
   spec: name => `import { render, screen } from '@testing-library/react'
   import ${name} from './${name}'
   describe('${name}', () => {
-    it('should render componente with ${name}', () => {
-      ...
+    it('renders', () => {
+      render(<${name} />)
+      expect(screen.getByText('${name}')).toBeInTheDocument()
     })
   }})`,
   stories: name => `import ${name} from './${name}'
   export default {
-    title: '${name}'
+    title: '${name}',
     component: ${name}
-  }`,
+  }
+
+  const Template = args => <${name} {...args} />
+
+  export const Default = Template.bind({})
+  Default.args = {}`,
 }
 
 const questions = [
   {
     type: 'input',
     name: 'name',
-    message:
-      'Which function and file would you like to create? \nPlease seperate by comma.',
+    message: 'Which function and file would you like to create?',
   },
   {
     type: 'checkbox',
@@ -41,8 +46,13 @@ const questions = [
 ]
 
 inquirer.prompt(questions).then(answers => {
+  // answers = { name: 'Header', fileTypes: ['component', 'spec']}
+
+  // iterate over fileTypes array from answer
   answers.fileTypes.forEach(fileType => {
+    // get value (our template function) from each fileType
     const templateFunction = templates[fileType]
+    // call function and save string
     const fileString = templateFunction(answers.name)
     writeFile(answers.name, fileType, fileString)
   })
